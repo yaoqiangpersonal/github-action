7abe63ed868c73d248d8f97a80cbe6449504769b
wei liang <562456140@qq.com>
2022-11-25 23:01:37 +0800

    first commit

---
+++ src/main/java/com/example/githubaction/ActionsController.java
@@ -0,0 +1,13 @@
+package com.example.githubaction;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ActionsController {
+
+	@GetMapping("actions")
+	public String actions(){
+		return "use github actions success----";
+	}
+}
---
+++ src/test/java/com/example/githubaction/GithubActionApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.githubaction;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
